15d1cfddf1b96cb4b655896bc98078fb7b6e9240
Tseng, I-Hsiu <43575082+TIS-JOEY@users.noreply.github.com>
2020-10-30 01:16:39 +0800

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,68 @@
-import { Component } from '@angular/core';
-import { City } from './model/city';
+import { Category } from './model/category';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './model/product';
+import {MenuItem} from 'primeng/api';
+import {
+  enable as enableDarkMode,
+  disable as disableDarkMode,
+  auto as followSystemColorScheme,
+  exportGeneratedCSS as collectCSS,
+} from 'darkreader';
+import { Function } from './model/function';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  cities: City[];
+export class AppComponent implements OnInit {
+  
+  public apList: MenuItem[];
+  public activeAp: MenuItem;
+  public products: MenuItem[];
+  public selectedProduct: Product;
+  public categories: Category[] = [];
+  public functions: Function[] = [];
+  
+  ngOnInit(): void {
+    this.apList = [
+      {label: '', icon: 'pi pi-th-large'},
+      {label: 'Part DCOP'},
+      {label: 'LowYieldAnalysis'},
+      {label: 'N5 DailyReport'},
+      {label: 'Lot WAT'},
+      {label: 'Part WAT'},
+      {label: '+'}
+  ];
+  this.activeAp = this.apList[0];
 
-    countries: any[];
+    this.products = [
+      { label: 'IPOP', icon: 'pi pi-refresh', },
+      { label: 'iDS', icon: 'pi pi-times', },
+      { label: 'IEDA2', icon: 'pi pi-external-link', },
+      { label: 'NTO System', icon: 'pi pi-upload', },
+      { label: 'Litho Engineer', icon: 'pi pi-refresh', },
+    ];
 
-    selectedCity: City;
+    this.categories = [
+      { title: 'XD', 'description': 'No Doubt'},
+      { title: 'ABC', 'description': 'No Doubt'},
+      { title: 'QQQ', 'description': 'No Doubt'},
+    ];
 
-    selectedCountries: any[];
+    this.functions = [
+      { title: 'Part DCOP', 'description': 'Analyze a lot of things, like XD', icon: 'pi-apple' },
+      { title: 'DailyReport', 'description': 'Analyze a lot of things, like XD', icon: 'pi-calendar' },
+      { title: 'LowYieldAnalysis', 'description': 'Analyze a lot of things, like XD', icon: 'pi-chart-line' },
+      { title: 'Part DCOP', 'description': 'Analyze a lot of things, like XD', 'icon': 'pi-chart-bar' },
+      { title: 'DailyReport', 'description': 'Analyze a lot of things, like XD', icon: 'pi-camera' },
+      { title: 'LowYieldAnalysis', 'description': 'Analyze a lot of things, like XD', icon: 'pi-file' },
+    ]
 
-    constructor() {
-        this.cities = [
-            {name: 'New York', code: 'NY'},
-            {name: 'Rome', code: 'RM'},
-            {name: 'London', code: 'LDN'},
-            {name: 'Istanbul', code: 'IST'},
-            {name: 'Paris', code: 'PRS'}
-        ];
-
-        this.countries = [
-            {name: 'Australia', code: 'AU'},
-            {name: 'Brazil', code: 'BR'},
-            {name: 'China', code: 'CN'},
-            {name: 'Egypt', code: 'EG'},
-            {name: 'France', code: 'FR'},
-            {name: 'Germany', code: 'DE'},
-            {name: 'India', code: 'IN'},
-            {name: 'Japan', code: 'JP'},
-            {name: 'Spain', code: 'ES'},
-            {name: 'United States', code: 'US'}
-        ];
-    }
+    enableDarkMode({
+      brightness: 100,
+      contrast: 90,
+      sepia: 10,
+  });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,18 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {ButtonModule} from 'primeng/button';
 import {TabViewModule} from 'primeng/tabview';
+import {CardModule} from 'primeng/card';
+import {MenuModule} from 'primeng/menu';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TabMenuModule} from 'primeng/tabmenu';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     ScrollPanelModule,
     ListboxModule,
@@ -23,7 +29,10 @@ import {TabViewModule} from 'primeng/tabview';
     FormsModule,
     ListboxModule,
     ButtonModule,
-		TabViewModule,
+    TabViewModule,
+    CardModule,
+    MenuModule,
+    TabMenuModule
   ],
   exports: [],
   providers: [],
